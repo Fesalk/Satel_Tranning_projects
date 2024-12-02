aaf7d51da9985158a4386bc34f7ec00d7b1cdfc7
Fesalk <zaikhan17@gmail.com>
2024-12-02 11:01:43 +0530

    Add files via upload

---
+++ Tranning/script.js
@@ -0,0 +1,13 @@
+// let score = prompt("Enter your %");
+// let grade;
+// if (score >= 90) {
+//     console.log("grade" = "A")
+// } else if (score >= 80) {
+//   console.log("grade" = "B")
+// } else if (score >= 70) {
+//   console.log("grade" = "C")
+// } else {
+//   console.log("grade" = "D");
+// }
+
+// console.log("Your Grade is:", grade);
